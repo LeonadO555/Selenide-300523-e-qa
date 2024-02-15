14f173612ee6fd33821294485bb4352bf4c3b1b8
Tetiana Skvarnyk <t.skvarnikgmail.com>
2024-02-15 22:07:17 +0100

    Added LoginPage, LoginTest.java

--- src/test/java/e2e/TestBase.java
+++ src/test/java/e2e/TestBase.java
@@ -1,6 +1,5 @@
 package e2e;
 
-import com.codeborne.selenide.Config;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.logevents.SelenideLogger;
@@ -9,20 +8,21 @@ import io.qameta.allure.selenide.AllureSelenide;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+
 import static com.codeborne.selenide.Selenide.open;
 
 public class TestBase {
     @BeforeMethod
-    public void init(){
-        SelenideLogger.addListener("allure", new AllureSelenide());// собирает для aллюра
-        Configuration.reportsFolder = "test-result/reports";//кладется в репорт
+    public void init() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+        Configuration.reportsFolder = "test-result/reports";
         ScreenShooter.captureSuccessfulTests = true;
         Configuration.browserCapabilities = new ChromeOptions().addArguments("--remote-allow-origins=*");
         open("https://www.saucedemo.com/");
     }
 
     @AfterMethod
-    public  void closeBrowser(){
+    public void closeBrowser() {
         Selenide.closeWebDriver();
     }
 }
--- src/test/java/pages/LoginPage.java
+++ src/test/java/pages/LoginPage.java
@@ -1,10 +1,29 @@
 package pages;
 
-import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.SelenideElement;
+import io.qameta.allure.Step;
 
+import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$x;
 
-public class LoginPage extends BasePage{
-    private SelenideElement loginInput = $x("id = user-name");
+public class LoginPage extends BasePage {
+    private SelenideElement loginInput = $x("//*[@id='user-name']");
+    private SelenideElement passwordInput = $x("//*[@id='password']");
+    private SelenideElement loginButton = $x("//*[@id='login-button']");
+    private SelenideElement loginCredentials = $("[id='login_credentials']");
+    private SelenideElement loginPassword = $x("//*[@class='login_password']");
+
+    @Step("Login as user with data username: {username} ,password: {password}")
+    public void login(String username, String password) {
+        loginInput.sendKeys(username);
+        passwordInput.sendKeys(password);
+        loginButton.click();
+    }
+
+    @Step("Confirm login successful")
+    public void confirmLogin() {
+        loginCredentials.shouldBe(Condition.hidden);
+        loginPassword.shouldNotBe(Condition.visible);
+    }
 }
