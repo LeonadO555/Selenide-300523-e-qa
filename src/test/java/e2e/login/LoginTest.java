350de3639089f275002c45a8ee18e47bab4dbd08
AliaksandrPakhamovich <alexberr9501@gmail.com>
2024-02-22 00:09:24 +0100

    Described firs selenide tests

---
+++ src/test/java/e2e/login/LoginTest.java
@@ -0,0 +1,67 @@
+package e2e.login;
+
+import e2e.TestBase;
+import io.qameta.allure.*;
+import org.testng.annotations.Test;
+import pages.LoginPage;
+
+public class LoginTest extends TestBase {
+    LoginPage loginPage;
+    String password = "secret_sauce";
+
+    public void positiveTestMethod(String username) {
+        loginPage = new LoginPage();
+        loginPage.login(username, password);
+        loginPage.confirmLogin();
+    }
+
+    @Epic("Login")
+    @Feature("Positive login")
+    @Description("Login as standard_user with valid password")
+    @AllureId("4")
+    @Severity(SeverityLevel.BLOCKER)
+    @Test(description = "Login as standard_user with valid password")
+    public void loginAsStandardUser() {
+        positiveTestMethod("standard_user");
+    }
+
+    @Epic("Login")
+    @Feature("Positive login")
+    @Description("Login as problem_user with valid password")
+    @AllureId("3")
+    @Severity(SeverityLevel.BLOCKER)
+    @Test(description = "Login as problem_user with valid password")
+    public void loginAsProblemUser() {
+        positiveTestMethod("problem_user");
+    }
+
+    @Epic("Login")
+    @Feature("Positive login")
+    @Description("Login as performance_glitch_user with valid password")
+    @AllureId("2")
+    @Severity(SeverityLevel.BLOCKER)
+    @Test(description = "Login as performance_glitch_user with valid password")
+    public void loginAsPerformanceGlitchUser() {
+        positiveTestMethod("performance_glitch_user");
+    }
+
+    @Epic("Login")
+    @Feature("Positive login")
+    @Description("Login as error_user with valid password")
+    @AllureId("1")
+    @Severity(SeverityLevel.BLOCKER)
+    @Test(description = "Login as error_user with valid password")
+    public void loginAsErrorUser() {
+        positiveTestMethod("error_user");
+    }
+
+    @Epic("Login")
+    @Feature("Positive login")
+    @Description("Login as visual_user with valid password")
+    @AllureId("5")
+    @Severity(SeverityLevel.BLOCKER)
+    @Test(description = "Login as visual_user with valid password")
+    public void loginAsVisualUser() {
+        positiveTestMethod("visual_user");
+    }
+}
--- src/test/java/pages/LoginPage.java
+++ src/test/java/pages/LoginPage.java
@@ -1,12 +1,30 @@
 package pages;
 
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.SelenideElement;
+import io.qameta.allure.Step;
 
 import static com.codeborne.selenide.Selenide.$x;
 
 public class LoginPage extends BasePage {
-    private SelenideElement userNameInput = $x("");
-    private SelenideElement passwordInput = $x("");
-    private SelenideElement loginInput = $x("");
-    private SelenideElement textField = $x("");
+    private SelenideElement loginInput = $x("//*[@id='user-name']");
+    private SelenideElement passwordInput = $x("//*[@id='password']");
+    private SelenideElement loginButton = $x("//*[@id='login-button']");
+    private SelenideElement loginCredentials = $x("//*[@id='login_credentials']");
+    private SelenideElement loginPassword = $x("//*[@class='login_password']");
+
+    @Step("Login as user with data username: {username}, {password}")
+    public void login(String username, String password) {
+        loginInput.sendKeys(username);
+        passwordInput.sendKeys(password);
+        loginButton.click();
+    }
+
+    @Step("Confirm login successful")
+    public void confirmLogin() {
+        loginCredentials.shouldBe(Condition.hidden);
+        loginPassword.shouldNotBe(Condition.visible);
+    }
+
+
 }
