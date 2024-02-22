a38ca2987854046fbd17146a6f032c53ff9fb7c3
OksanaPovkh <133179018+OksanaPovkh@users.noreply.github.com>
2024-02-22 14:56:03 -0500

    Added webrivermanadger, descrided, IframeTest.java

--- src/test/java/e2e/TestBase.java
+++ src/test/java/e2e/TestBase.java
@@ -6,6 +6,7 @@ import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import com.codeborne.selenide.testng.ScreenShooter;
+import io.github.bonigarcia.wdm.WebDriverManager;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterMethod;
@@ -16,6 +17,7 @@ import static com.codeborne.selenide.Selenide.open;
 public class TestBase {
     @BeforeMethod
     public void init() {
+        WebDriverManager.chromedriver().setup();
         SelenideLogger.addListener("allure", new AllureSelenide());
         Configuration.reportsFolder = "test-result/reports";
         ScreenShooter.captureSuccessfulTests = true;
