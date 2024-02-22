558e09f1cd620031a939bc697e0e096d9232600d
Georgiy <g.manolov@web.de>
2024-02-22 22:07:05 +0100

    ProductsPage.java

--- src/test/java/pages/ProductsPage.java
+++ src/test/java/pages/ProductsPage.java
@@ -1,25 +1,66 @@
 package pages;
 
+import com.codeborne.selenide.ElementsCollection;
 import com.codeborne.selenide.SelenideElement;
+import com.codeborne.selenide.selector.ByText;
 
-import static com.codeborne.selenide.Selenide.$x;
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$;
 
-public class ProductsPage extends BasePage {
-
-    private final SelenideElement addProductsButton = $x("//*[@id='item_4_title_link']");
-    private final SelenideElement addToCartButton = $x("//*[@class='btn btn_primary btn_small btn_inventory']");
-    private final SelenideElement loginButton = $x("//*[@id='login-button']");
-
-    public void product() {
-        addProductsButton.pressTab();
+public class ProductsPage {
 
 
+    ElementsCollection inventoryItem = $$("[@class='inventory_item']");
+    ElementsCollection inventoryItemName = $$("[class='inventory_item_name ']");
+    ElementsCollection inventoryItemPrice = $$("[class='inventory_item_price']");
+    SelenideElement shoppingCartLink = $("[class=shopping_cart_link']");
+
+
+    public void openCartPage() {
+        shoppingCartLink.shouldBe(visible, enabled)
+                .click();
     }
 
-    public void cart() {
-        addToCartButton.click();
+    public void openProductPage(String[] productNames) {
+        for (String productName : productNames) {
+            $(new ByText(productName)).shouldHave(text(productName))
+                    .shouldBe(visible).click();
+        }
     }
 
+    public void waitForVisibilityAllItems() {
+        int numberOfItem = inventoryItem.size();
+        for (int i = 0; i < numberOfItem; i++) {
+            inventoryItem.get(i).shouldBe(visible);
+        }
+    }
+
+    public String[] getAllItemName() {
+        waitForVisibilityAllItems();
+        return inventoryItemName.texts().toArray(new String[0]);
+    }
+
+    public String[] getAllItemPrice() {
+        waitForVisibilityAllItems();
+        return inventoryItemPrice.texts().toArray(new String[0]);
+    }
+
+    public void clickAddToCart(String[] productNames) {
+        for (String productName : productNames) {
+            SelenideElement addCartButton = $("[data-test='add-to-cart-" + productName.replace(" ", "-").toLowerCase() + "']");
+            addCartButton.shouldBe(visible, enabled)
+                    .shouldHave(text("Add to cart"))
+                    .click();
+        }
+    }
+
+    public void clickRemoteToCart(String[] productNames) {
+        for (String productName : productNames) {
+            SelenideElement remoteCartButton = $("[data-test='add-to-cart-" + productName.replace(" ", "-").toLowerCase() + "']");
+            remoteCartButton.shouldBe(visible, enabled)
+                    .shouldHave(text("Remote"))
+                    .click();
+        }
+    }
 }
-
-
