0a937cbfa45920d3bb9bb67aa41880a782537200
AliaksandrPakhamovich <alexberr9501@gmail.com>
2024-02-25 22:52:54 +0100

    Added Iframe.java and IframeTest.java

---
+++ src/test/java/e2e/iframe/IframeTest.java
@@ -0,0 +1,31 @@
+package e2e.iframe;
+
+import com.codeborne.selenide.Configuration;
+import e2e.elements.Iframe;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.Test;
+
+import static com.codeborne.selenide.Selenide.closeWebDriver;
+import static com.codeborne.selenide.Selenide.open;
+import static org.testng.Assert.assertEquals;
+
+public class IframeTest {
+
+    Iframe iframe;
+
+    @Test
+    public void iframeTest() {
+        Configuration.browserCapabilities = new ChromeOptions().addArguments("--remote-allow-origins=*");
+        open("https://demoqa.com/frames");
+        String expectedHeadingText = "This is sample page";
+
+        iframe = new Iframe();
+        String bigHeadingText = iframe.getSampleHeadingBig();
+        assertEquals(bigHeadingText, expectedHeadingText, bigHeadingText + "is not equal" + expectedHeadingText);
+
+        String smallHeadingText = iframe.getSampleHeadingSmall();
+        assertEquals(smallHeadingText, expectedHeadingText, smallHeadingText + "is not equal" + expectedHeadingText);
+
+        closeWebDriver();
+    }
+}
