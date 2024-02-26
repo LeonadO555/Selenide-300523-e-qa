9312f3c06f2cd6127e9b54fb3d617e9e02c9feab
OLEKSANDR RASHEVCHENKO <rashevchenkoo@gmail.com>
2024-02-26 18:01:09 +0100

    Described ProductPaga,YourCardPage

--- src/test/java/pages/ProductPaga.java
+++ src/test/java/pages/ProductPaga.java
@@ -1,4 +1,41 @@
 package pages;
 
+import com.codeborne.selenide.SelenideElement;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selenide.$;
+
 public class ProductPaga {
+    SelenideElement inventoryItemContainer = $("[class='inventory_details_container']");
+    SelenideElement inventoryItemNameOnProductPage = $("[class='inventory_details_name large_size']");
+    SelenideElement inventoryItemDescriptionOnProductPage = $("[class='inventory_details_desc large_size']");
+    SelenideElement inventoryItemPrice = $("[class='inventory_details_price']");
+    SelenideElement shoppingCartLink = $("[class='shopping_cart_link']");
+
+    public void clickAddToCartOnProductPage(String productName) {
+        SelenideElement addCartButton = $("[data-test='add-to-cart-" + productName.replace(" ", "-").toLowerCase() + "']");
+        addCartButton.shouldBe(visible, enabled).shouldHave(text("Add to cart")).click();
+    }
+
+    public void clickRemoveFromCartOnProductPage(String productName) {
+        SelenideElement removeButton = $("[data-test='remove-" + productName.replace(" ", "-").toLowerCase() + "']");
+        removeButton.shouldBe(visible, enabled).shouldHave(text("Remove")).click();
+
+    }
+
+    public String getItemPrice() {
+        waitForVisibility();
+        return inventoryItemPrice.text();
+    }
+
+    public String getItemName() {
+        waitForVisibility();
+        return inventoryItemNameOnProductPage.text();
+    }
+
+    public void waitForVisibility() {
+        inventoryItemContainer.shouldBe(visible);
+    }
+
+
 }
