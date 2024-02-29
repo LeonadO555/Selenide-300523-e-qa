8fe39ee30fb9db0230c2b4ca7fe4ff0fa790945e
LeonadO555 <lmikhailov@supplyshift.net>
2024-02-29 22:22:59 +0400

    Changed a lot of things

--- src/test/java/pages/YourCartPage.java
+++ src/test/java/pages/YourCartPage.java
@@ -7,50 +7,51 @@ import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$$;
 
-public class YourCartPage extends ProductsPage {
+public class YourCartPage {
+    ElementsCollection inventoryItemOnYourCardPage = $$("[class='cart_item']");
+    ElementsCollection inventoryItemNameOnYourCardPage = $$("[class='inventory_item_name']");
+    ElementsCollection inventoryItemPrice = $$("[class='inventory_item_price']");
+    SelenideElement checkoutButton = $("[id='checkout']");
+    SelenideElement continueShoppingButton = $("[id='continue-shopping']");
 
-    public ElementsCollection listCartItems = $$("[class='cart_item']");
-    public ElementsCollection inventoryItemName = $$("[class='inventory_item_name']");
-    public ElementsCollection inventoryItemPrice = $$("[class='inventory_item_price']");
-    public SelenideElement checkoutButton = $("[id='checkout']");
-
-    public void waitForVisibilityAllItems() {
-        int numberOfItem = listCartItems.size();
-        for (int i = 0; i < numberOfItem; i++) {
-            listCartItems.get(i).shouldBe(visible);
+    public void clickRemoveFromCart(String[] productNames) {
+        for (String productName : productNames) {
+            SelenideElement removeButton = $("[data-test='remove-" + productName.replace(" ", "-").toLowerCase() + "']");
+            removeButton.shouldBe(visible, enabled).shouldHave(text("Remove")).click();
         }
     }
 
     public String[] getAllItemName() {
-        waitForVisibilityAllItems();
-        return listCartItems.texts().toArray(new String[0]);
+        waitFroVisibilityAllItems();
+        return inventoryItemNameOnYourCardPage.texts().toArray(new String[0]);
     }
 
-    public String[] getNameCart() {
-        waitForVisibilityAllItems();
-        return inventoryItemName.texts().toArray(new String[0]);
-    }
-
-    public String[] getCartPrice() {
-        waitForVisibilityAllItems();
+    public String[] getAllItemPrice() {
+        waitFroVisibilityAllItems();
         return inventoryItemPrice.texts().toArray(new String[0]);
     }
 
-    public void clickRemoveFromCart(String productName) {
-        SelenideElement removeButton = $("[data-test='remove-" + productName.replace(" ", "-").toLowerCase() + "']");
-        removeButton.shouldBe(visible, enabled)
-                .shouldHave(text("Remove"))
-                .click();
+    public void waitFroVisibilityAllItems() {
+        int numberOfItem = inventoryItemOnYourCardPage.size();
+        for (int i = 0; i < numberOfItem; i++) {
+            inventoryItemOnYourCardPage.get(i).shouldBe(visible);
+        }
     }
 
-    public void clickContinueShopping(String productName) {
-        SelenideElement continueShoppingButton = $("[data-test='continue-shopping-" + productName.replace(" ", "-").toLowerCase() + "']");
-        continueShoppingButton.shouldBe(visible, enabled)
-                .shouldHave(text("Continue-shopping"))
-                .click();
+    public void waitFroInVisibilityAllItems() {
+        int numberOfItem = inventoryItemOnYourCardPage.size();
+        for (int i = 0; i < numberOfItem; i++) {
+            inventoryItemOnYourCardPage.get(i).shouldBe(hidden);
+        }
     }
 
-    public void clickCheckout() {
+    public void clickOnCheckoutButton() {
+        checkoutButton.shouldBe(visible);
+        checkoutButton.click();
+    }
+
+    public void clickOnContinueShoppingButton() {
+        continueShoppingButton.shouldBe(visible);
         checkoutButton.click();
     }
 }
